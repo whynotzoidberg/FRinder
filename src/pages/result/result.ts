273da34c3d26e874e9aa23f5f06f7e108417059c
Till Weißer <e1527667@student.tuwien.ac.at>
2018-06-23 17:54:02 +0200

    Beginne mit dem Routen Link

--- src/pages/result/result.ts
+++ src/pages/result/result.ts
@@ -24,7 +24,6 @@ export class ResultPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad ResultPage');
     this.likedCards = this.navParams.get('likedCards');
-    console.log(this.likedCards);
   }
 
   restart(){
@@ -32,4 +31,29 @@ export class ResultPage {
     });
   }
 
+  createMapLink() {
+
+    // https://maps.googleapis.com/maps/api/directions/json?origin=47.994939,7.842517&mode=walking&destination=47.994939,7.842517
+    //   // &waypoints=optimize:true%7C47.9763016,7.8177525%7C47.9848895,7.8541669&key=AIzaSyDRMVqSJjQJ2bnodfTwNcIJy1wpXiYYaYE
+
+
+   // https://maps.googleapis.com/maps/api/directions/json?origin=47.994939,7.842517&mode=walking&destination=
+    // 47.9984746,7.8293129&waypoints=optimize:trueG.9961777,7.8381631G.9955918,7.8118858%&key=AIzaSyDRMVqSJjQJ2bnodfTwNcIJy1wpXiYYaYE
+
+    let url:string = 'https://maps.googleapis.com/maps/api/directions/json?origin=47.994939,7.842517&mode=walking&destination=';
+
+    let dest = this.likedCards.pop();
+    url +=  dest.lat + "," + dest.lng + "&waypoints=optimize:true%";
+
+    this.likedCards.forEach(function (value) {
+      console.log(value);
+      url += value.lat + "," + value.lng + "%7C";
+    });
+
+    url +=  "&key=AIzaSyDRMVqSJjQJ2bnodfTwNcIJy1wpXiYYaYE";
+
+
+    console.log(url);
+
+  };
 }
